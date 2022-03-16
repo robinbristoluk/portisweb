e205d193399ac5204910230dd447bfccf4d5152a
rob <robinbristoluk@yahoo.o.uk>
2022-03-16 20:10:14 +0000

    Added custom document

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Portisweb</title>
+      </Head>
+      <div className='container mx-auto'>
+        <Component {...pageProps} />
+      </div>
+    </>
+  );
 }
 
 export default MyApp
