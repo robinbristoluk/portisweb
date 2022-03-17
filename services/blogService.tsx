0bbdf0414399d9161f36f49b42f90b21aca8445b
Rob <robinbristoluk@yahoo.co.uk>
2022-03-17 07:36:13 +0000

    Added more to blog service

--- services/blogService.tsx
+++ services/blogService.tsx
@@ -1,7 +1,37 @@
 import { BlogSummary } from "../types/BlogSummary";
 
-const blogs: BlogSummary[] = [];
+const blogs: BlogSummary[] = [
+    {
+        slug: 'blog_1',
+        title: 'Blog 1',
+    },
+    {
+        slug: 'blog_2',
+        title: 'Blog 2',
+    },
+    {
+        slug: 'blog_3',
+        title: 'Blog 3',
+    }
+];
 
-const getBlogs = (): Promise<BlogSummary[]> => {
+export const getBlogs = (): Promise<BlogSummary[]> => {
     return Promise.resolve(blogs);
 }
+
+export const getFilteredBlogs = (query: object): Promise<BlogSummary[]> => {
+    const parsedQuery = Object.entries(query);
+    const filtered = blogs.filter((blog) => {
+        parsedQuery.forEach(([key, value]) => {
+            if (blog[key as keyof BlogSummary] !== value) { return false; } 
+        });
+
+        return true;
+    });
+
+    return Promise.resolve(filtered);
+}
+
+export const getBlogBySlug = async (slug: string): Promise<BlogSummary|undefined> => {
+    return blogs.find(b => b.slug === slug);
+} 
